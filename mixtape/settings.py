5e87769672067f96465e3256ca3168cfcea92088
Mia Frank <31712634+miafrank@users.noreply.github.com>
2023-11-28 18:15:03 -0600

    Use postgresql over sqlite

--- mixtape/settings.py
+++ mixtape/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'mixtape.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mixtape',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost'
     }
 }
 
