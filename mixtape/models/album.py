44b31a384000e44b057142e53b37184680a55bdb
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-01-08 15:54:20 -0600

    Update album and track models to use ManyToManyField for artists

--- mixtape/models/album.py
+++ mixtape/models/album.py
@@ -5,4 +5,4 @@ from artist import Artist
 class Album(models.Model):
     name = models.CharField(max_length=50)
     total_tracks = models.IntegerField(MinValueValidator=1)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    artists = models.ManyToManyField(Artist)
--- mixtape/models/track.py
+++ mixtape/models/track.py
@@ -5,5 +5,5 @@ from album import Album
 
 class Track(models.Model):
     name = models.CharField(max_length=50)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    artists = models.ManyToManyField(Artist)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
