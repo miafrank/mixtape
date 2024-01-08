aebe364376ee0ecbfa9942677c09a3ab36b05f61
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-01-08 17:01:46 -0600

    WIP: Setup spotify client tests

---
+++ mixtape/client/spotify/spotify_client.py
@@ -0,0 +1,12 @@
+import spotipy
+
+
+class SpotifyClient():
+    def client():
+        # Authorize spotify
+        # Create .env to store creds
+        # Pull in dotenv lib
+        # Create Client(Inherits from spotify lib)
+        # Init will authorize client
+        # top_track will be called from /track
+        pass
