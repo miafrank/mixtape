59fd3c60c6715b11daefc04e912f640670a97a4b
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-01-09 16:16:22 -0600

    Add album and track associations to artist

--- mixtape/models/artist.py
+++ mixtape/models/artist.py
@@ -3,3 +3,5 @@ from django.db import models
 
 class Artist(models.Model):
     name = models.CharField(max_length=50)
+    album = models.ForeignKey("Album", on_delete=models.CASCADE)
+    track = models.ForeignKey("Track", on_delete=models.CASCADE)
