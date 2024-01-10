ba702370204e4ab021d4ab7a13dc6dc7096ff567
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-01-10 17:42:18 -0600

    Update database name in create.py

--- mixtape/db/create.py
+++ mixtape/db/create.py
@@ -2,4 +2,4 @@ import psycopg
 
 with psycopg.connect("user=postgres", autocommit=True) as conn:
     cur = conn.cursor()
-    conn.execute("CREATE DATABASE test")
+    conn.execute("CREATE DATABASE mixtape")
