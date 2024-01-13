eafbb34dc8876dcd8de0e6723137ea3b3f72b98c
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-01-13 17:52:50 -0600

    Refactor Track model relationships

--- mixtape/models/track.py
+++ mixtape/models/track.py
@@ -1,9 +1,8 @@
 from django.db import models
-from artist import Artist
-from album import Album
 
 
 class Track(models.Model):
     name = models.CharField(max_length=50)
-    artists = models.ManyToManyField(Artist)
-    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    artists = models.ForeignKey(
+        "Artist", on_delete=models.CASCADE, related_name="artist_track")
+    album = models.OneToOneField("Album", on_delete=models.CASCADE)
